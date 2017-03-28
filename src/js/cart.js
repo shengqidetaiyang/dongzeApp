e7ccee7ab8031fa5a74ade6743e200e42308039b
shengqidetaiyang <494407127@qq.com>
2017-03-28 18:37:57 +0800

    3-28 finished
    文章列表和文章详情页
    购物车js书写中

---
+++ src/js/cart.js
@@ -0,0 +1,13 @@
+/* 
+* @Author: Marte
+* @Date:   2017-03-28 15:01:50
+* @Last Modified by:   Marte
+* @Last Modified time: 2017-03-28 18:15:33
+*/
+
+$(document).ready(function(){
+    $(".goodsList .select-icon").on("touchstart",function(){
+        $(this).toggleClass('sel-active');
+    })
+    
+});
--- src/js/goodDetail.js
+++ src/js/goodDetail.js
@@ -2,7 +2,7 @@
 * @Author: Marte
 * @Date:   2017-03-27 14:59:06
 * @Last Modified by:   Marte
-* @Last Modified time: 2017-03-27 17:05:17
+* @Last Modified time: 2017-03-28 10:43:47
 */
 
 $(document).ready(function(){
@@ -12,10 +12,31 @@ $(document).ready(function(){
         // 如果需要分页器
         pagination: '.swiper-pagination',
       }) 
+      var index = 1;
       $(".tabBox span").on("touchstart",function(event){
         var e = event || window.event;
         $(this).addClass("tap").siblings().removeClass("tap");
         var index = $(e.target).attr("data-id");
         $(".content>div").eq(index).show().siblings().hide();
       })    
+      $(".buySoon").on("touchstart",function(){
+        $(".numChoose").fadeIn();
+      })
+      $(".close").on("touchstart",function(){
+        $(".numChoose").fadeOut();
+      })
+      $(".numChoose .select span").on("touchstart",function(){
+        $(this).addClass("s-this").siblings().removeClass("s-this");
+      })
+      $(".numChoose .minus ").on("touchstart",function(){
+        index--;
+        if(index <=1){
+            index = 1;
+        }
+        $(".number").text(index);
+      })
+      $(".numChoose .plus").on("touchstart",function(){
+        index++
+        $(".number").text(index);
+      })
 });
